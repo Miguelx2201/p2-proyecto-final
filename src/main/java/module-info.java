1a07c0340f666852c14c6fae702df54b1a96145e
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2026-05-10 20:19:07 -0500

    lombok dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.edu.uniquindio.poo.p2proyectofinal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens co.edu.uniquindio.poo.p2proyectofinal to javafx.fxml;
