e5c0e38db32721e334256f3d6d32fcaeae8740b2
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2026-05-10 20:26:42 -0500

    Create Evento.java

---
+++ src/main/java/co/edu/uniquindio/poo/p2proyectofinal/model/Evento.java
@@ -0,0 +1,27 @@
+package co.edu.uniquindio.poo.p2proyectofinal.model;
+
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Evento {
+    private String idEvento;
+    private String nombre;
+    private Categoria categoria;
+    private LocalDateTime fechaHora;
+    private EstadoEvento estadoEvento;
+    private String descripcion;
+    private String ciudad;
+    private String politicas;
+    private List<Compra> compras;
+    private Recinto recinto;
+    
+}
