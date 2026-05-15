08221e0817bf7f7da3fd6c279ebc61685ca4e4a8
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2026-05-15 16:47:40 -0500

    Create CompraIncidenciaState.java
    Se añadio el estado incidencia para una compra

---
+++ src/main/java/co/edu/uniquindio/poo/p2proyectofinal/model/CompraIncidenciaState.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.poo.p2proyectofinal.model;
+
+public class CompraIncidenciaState implements IEstadoCompra{
+    @Override
+    public void pagar(IMetodoPago metodoPago) throws ProyectoException {
+        throw new ProyectoException("Ha ocurrido una incidencia con la compra. Comuniquese con el administrador.");
+    }
+
+    @Override
+    public void cancelar() throws ProyectoException {
+        throw new ProyectoException("Ha ocurrido una incidencia con la compra. No es posible cancelarla.");
+    }
+}
