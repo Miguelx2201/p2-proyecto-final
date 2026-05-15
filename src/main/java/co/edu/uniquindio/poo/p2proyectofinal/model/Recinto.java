270cc1e07f67442ed20780809c1db38a9de6954a
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2026-05-15 15:44:16 -0500

    Update Recinto.java
    Se intento añadir una simulación de como una llave primaria con sus dos caracteristicas principales, autoincrementable y no nula

--- src/main/java/co/edu/uniquindio/poo/p2proyectofinal/model/Recinto.java
+++ src/main/java/co/edu/uniquindio/poo/p2proyectofinal/model/Recinto.java
@@ -7,13 +7,22 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
 public class Recinto {
+    private static int numRecintos=0;
     private String idRecinto;
     private String nombre;
     private String direccion;
     private String ciudad;
     private List<Zona> zonas;
+
+    public Recinto(String nombre, String direccion, String ciudad, List<Zona> zonas) {
+        //Aqui se intenta simular una llave primaria como en una base de datos, se intenta que cumpla con
+        // las caracteristicas de llave primaria, es decir, autoincrementable y no nula.
+        this.idRecinto = String.valueOf(++numRecintos);
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.zonas = zonas;
+    }
 }
