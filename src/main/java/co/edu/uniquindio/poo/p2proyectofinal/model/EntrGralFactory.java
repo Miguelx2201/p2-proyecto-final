932f3b3ef579cbd9fae9939babce8f96d313b655
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2026-05-16 09:01:30 -0500

    eliminar factory repetido

--- src/main/java/co/edu/uniquindio/poo/p2proyectofinal/model/EntrGralFactory.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo.p2proyectofinal.model;
-
-public class EntrGralFactory implements EntradaFactory {
-
-    @Override
-    public IEntrada createEntrada( Zona zona, Asiento asiento, EstadoEntrada estadoEntrada, String descripcion, double precio) {
-        return new EntradaGral(zona,asiento,estadoEntrada,descripcion,precio);
-    }
-}
--- src/main/java/co/edu/uniquindio/poo/p2proyectofinal/model/EntrPrefFactory.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo.p2proyectofinal.model;
-
-public class EntrPrefFactory implements EntradaFactory{
-    @Override
-    public IEntrada createEntrada(Zona zona, Asiento asiento, EstadoEntrada estadoEntrada, String descripcion, double precio) {
-        return new EntradaPreferencial(zona,asiento,estadoEntrada,descripcion,precio);
-    }
-}
